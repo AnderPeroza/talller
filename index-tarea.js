327156dd4b0962f5eba548ec2f156e511a694016
AnderPeroza <109151281+AnderPeroza@users.noreply.github.com>
2022-08-16 18:45:21 -0400

    Add files via upload

--- index-tarea.js
+++ index-tarea.js
@@ -16,10 +16,15 @@ const deleteTask = (id) =>{
     const elementToDelete = document.getElementById(id)
     taskListElement.removeChild(elementToDelete)
 }
+const taskListElement = document.getElementById("task-list")
 
 const formElement= document.getElementById("task-form")
+const selectElement = document.getElementById("taskPriority")
+const inputElement = document.getElementById("taskName")
+const taskDateElement = document.getElementById("taskDate")
 
-if(formElement){
+
+/* if(formElement){
     //modificar el evento
     
     formElement.addEventListener("submit",function(event){
@@ -86,7 +91,51 @@ if(formElement){
         } 
     })
 
-}
+} */
+const buttonAdd = document.getElementById("add-task-btn")
+
+buttonAdd.addEventListener("click",async()=>{
+    if(selectElement.value !== "" && inputElement.value !== "" && taskDateElement.value !== ""){
+        
+        const newTask = {
+            "name": inputElement.value,
+            "date": taskDateElement.value ,
+            "priority": selectElement.value
+        }
+        
+        const response = await fetch('https://graco-task-list.herokuapp.com/task', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify( newTask )
+        })
+        const data = await response.json()
+
+        if(response.status === 200){
+            Swal.fire(
+                'Exito!',
+                'Registro Exitoso',
+                'success'
+            )
+            init()
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: `${data.error}`,
+                footer: ''
+            })
+        }
+
+    }else{
+        Swal.fire(
+            'Campos Vacios',
+            'Debe llenar todos los campos requeridos!',
+            'error'
+        )
+    }
+})
 const init = async () =>{
     const token = localStorage.getItem('token')
 
@@ -99,6 +148,29 @@ const init = async () =>{
     const data = await reponse.json()
 
     console.log(data)
+    let index = 0
+    if(taskListElement.children.length > 0){
+        const ultimoHijo= taskListElement.children[taskListElement.children.length - 1]
+        index += parseInt(ultimoHijo.id)
+    }
+   
+    taskListElement.innerHTML +=`
+    <li id="${data.data[index]._id}"  class="list-group-item d-flex justify-content-between align-items-center"
+    style="word-break: keep-all;">
+        <div class="mx-2 text-start" style="flex: 1;">
+            <div class="fw-bold">${data.data[index].name}</div>
+            <div class="fw-bold">${data.data[index].date}</div>                        
+        </div>
+        <span class="badge bg-primary rounded-pill mx-1">${data.data[0].priority}</span>
+
+        <button onclick="deleteTask()" type="button" class="btn btn-outline-danger btn-sm">
+            <svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20px" height="20px">
+                <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z" />
+            </svg>
+        </button>
+    </li>
+    `
+    console
 }
 init()
 
